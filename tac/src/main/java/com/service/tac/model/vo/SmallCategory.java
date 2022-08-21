e33078830a9fa48cf50cfec85bf63f414d8e18c8
dnjswnsthd <dnjswnsthd@naver.com>
2022-08-21 18:47:54 +0900

    Add SmallCategoryVO

---
+++ tac/src/main/java/com/service/tac/model/vo/SmallCategory.java
@@ -0,0 +1,50 @@
+package com.service.tac.model.vo;
+
+// Table Name: large_category
+public class SmallCategory {
+	private int smallCategoryId; // SMALL_CATEGORY_ID
+	private String smallCategoryName; // SMALL_CATEGORY_NAME
+	private int largeCategoryId; // LARGE_CATEGORY_ID
+
+	public SmallCategory() {
+		super();
+	}
+
+	public SmallCategory(int smallCategoryId, String smallCategoryName, int largeCategoryId) {
+		super();
+		this.smallCategoryId = smallCategoryId;
+		this.smallCategoryName = smallCategoryName;
+		this.largeCategoryId = largeCategoryId;
+	}
+
+	public int getSmallCategoryId() {
+		return smallCategoryId;
+	}
+
+	public void setSmallCategoryId(int smallCategoryId) {
+		this.smallCategoryId = smallCategoryId;
+	}
+
+	public String getSmallCategoryName() {
+		return smallCategoryName;
+	}
+
+	public void setSmallCategoryName(String smallCategoryName) {
+		this.smallCategoryName = smallCategoryName;
+	}
+
+	public int getLargeCategoryId() {
+		return largeCategoryId;
+	}
+
+	public void setLargeCategoryId(int largeCategoryId) {
+		this.largeCategoryId = largeCategoryId;
+	}
+
+	@Override
+	public String toString() {
+		return "SmallCategory [smallCategoryId=" + smallCategoryId + ", smallCategoryName=" + smallCategoryName
+				+ ", largeCategoryId=" + largeCategoryId + "]";
+	}
+
+}
