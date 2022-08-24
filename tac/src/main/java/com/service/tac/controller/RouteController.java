71fc608bffdf4024f61f400ee2327e72cfd85e1a
USERMIC-9H64N6C\BIFF2020 <dnjswnsthd@naver.com>
2022-08-24 20:18:41 +0900

    Add getAllCardInfo 구현

--- tac/src/main/java/com/service/tac/controller/RouteController.java
+++ tac/src/main/java/com/service/tac/controller/RouteController.java
@@ -1,18 +1,37 @@
 package com.service.tac.controller;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.service.tac.model.service.CardService;
+import com.service.tac.model.vo.Card;
+
 @Controller
 public class RouteController {
+	
+	@Autowired
+	CardService cardService;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String intro() {
 		return "intro";
 	}
 
-	@RequestMapping(value = "/main", method = RequestMethod.GET)
-	public String main() {
+	@GetMapping(value = "/main")
+	public String main(Model model) {
+		try {
+			ArrayList <Card> list = cardService.getAllCardInfo();
+			model.addAttribute("list", list);
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
 		return "/main/main";
 	}
 
