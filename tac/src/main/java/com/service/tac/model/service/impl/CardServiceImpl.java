fc6c62ab7642693bdf4abb8e02826a4607c3b93e
RyuKiTak <aliverise@naver.com>
2022-09-05 14:50:48 +0900

    Update Card Img Horizon

--- tac/src/main/java/com/service/tac/model/service/impl/CardServiceImpl.java
+++ tac/src/main/java/com/service/tac/model/service/impl/CardServiceImpl.java
@@ -1,6 +1,10 @@
 package com.service.tac.model.service.impl;
 
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
@@ -9,12 +13,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.UUID;
 
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.io.FilenameUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -108,20 +112,34 @@ public class CardServiceImpl implements CardService {
 	@Override
 	public int registerCard_Image(Card card, MultipartFile imgfile, HttpServletRequest request) throws SQLException {
 		String root = request.getSession().getServletContext().getRealPath("/");
-		String path = root + "resources\\image\\card\\";
-
+		String path = "";
+		
 		UUID uuid = UUID.randomUUID();
         String extension = FilenameUtils.getExtension(imgfile.getOriginalFilename());
         String savingFileName = uuid + "." + extension; 
-		File copyFile = new File(path + savingFileName);
-		
-		// 일단 호라이즌도 넣어버리자
-		path = root + "resources\\image\\card_horizon\\";
-		File copyFile2 = new File(path + savingFileName);
-		
+        
 		try {
-			imgfile.transferTo(copyFile);
-			Files.copy(copyFile.toPath(), copyFile2.toPath(), StandardCopyOption.REPLACE_EXISTING); 
+			// 일단 호라이즌 넣기 // 가로는 바로 넣어도 됨
+	 		path = root + "resources\\image\\card_horizon\\";
+	 		File copyFile2 = new File(path + savingFileName);
+	 		// 가로에는 바로 저장
+			imgfile.transferTo(copyFile2);
+			
+			// 돌려서 넣어야함
+			path = root + "resources\\image\\card\\";
+			File copyFile = new File(path + savingFileName);
+			
+			BufferedImage oldImage = ImageIO.read(copyFile2);	
+			BufferedImage newImage = new BufferedImage(oldImage.getHeight(), oldImage.getWidth(), oldImage.getType());
+			Graphics2D graphics = (Graphics2D) newImage.getGraphics();
+			graphics.rotate(Math.toRadians(90), newImage.getWidth() / 2, newImage.getHeight() / 2);
+			graphics.translate((newImage.getWidth() - oldImage.getWidth()) / 2, (newImage.getHeight() - oldImage.getHeight()) / 2);
+			graphics.drawImage(oldImage, 0, 0, oldImage.getWidth(), oldImage.getHeight(), null);
+			// 파일 넣어주기
+			FileOutputStream fo = new FileOutputStream(copyFile);
+			ImageIO.write(newImage, extension, fo);
+			fo.close();
+			
 			card.setCardImg(savingFileName);
 			card.setCardImgHorizon(savingFileName);
 			return sqlSession.getMapper(CardMapper.class).registerCardImportImage(card);
@@ -137,7 +155,8 @@ public class CardServiceImpl implements CardService {
 	@Override
 	public int updateCard2(Card card, MultipartFile img, HttpServletRequest request) throws SQLException {
 		String root = request.getSession().getServletContext().getRealPath("/");
-		String path = root + "resources\\image\\card\\";
+		String path = "";
+		
 		// 기존 이미지는 삭제하기 
 		String deleteImg = card.getCardImg();
 		if ( !deleteImg.equals("card_none.png")) {
@@ -155,20 +174,34 @@ public class CardServiceImpl implements CardService {
 				file.delete();
 	        }
 		}
-        // 기존 이미지 추가
+		
+        // 새로운 이미지 추가
 		UUID uuid = UUID.randomUUID();
         String extension = FilenameUtils.getExtension(img.getOriginalFilename());
         String savingFileName = uuid + "." + extension; 
-        
-        path = root + "resources\\image\\card\\";
-		File copyFile = new File(path + savingFileName);
-		// 호라이즌
-		path = root + "resources\\image\\card_horizon\\";
-		File copyFile2 = new File(path + savingFileName);
 		
 		try {
-			img.transferTo(copyFile);
-			Files.copy(copyFile.toPath(), copyFile2.toPath(), StandardCopyOption.REPLACE_EXISTING); 
+			// 일단 호라이즌 넣기 // 가로는 바로 넣어도 됨
+	 		path = root + "resources\\image\\card_horizon\\";
+	 		File copyFile2 = new File(path + savingFileName);
+	 		// 가로에는 바로 저장
+	 		img.transferTo(copyFile2);
+			
+			// 돌려서 넣어야함
+			path = root + "resources\\image\\card\\";
+			File copyFile = new File(path + savingFileName);
+			
+			BufferedImage oldImage = ImageIO.read(copyFile2);	
+			BufferedImage newImage = new BufferedImage(oldImage.getHeight(), oldImage.getWidth(), oldImage.getType());
+			Graphics2D graphics = (Graphics2D) newImage.getGraphics();
+			graphics.rotate(Math.toRadians(90), newImage.getWidth() / 2, newImage.getHeight() / 2);
+			graphics.translate((newImage.getWidth() - oldImage.getWidth()) / 2, (newImage.getHeight() - oldImage.getHeight()) / 2);
+			graphics.drawImage(oldImage, 0, 0, oldImage.getWidth(), oldImage.getHeight(), null);
+			// 파일 넣어주기
+			FileOutputStream fo = new FileOutputStream(copyFile);
+			ImageIO.write(newImage, extension, fo);
+			fo.close();
+			
 			card.setCardImg(savingFileName);
 			card.setCardImgHorizon(savingFileName);
 			return sqlSession.getMapper(CardMapper.class).updateCard2(card);
